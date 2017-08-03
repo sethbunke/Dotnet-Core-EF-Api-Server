05ae35074b157a2665506ec1b4982d1a5ac8f2af
sethbunke <sjb_strat@hotmail.com>
2017-08-03 01:53:15 +0000

    set up context and connection info

--- Persistence/VegaDbContext.cs
+++ Persistence/VegaDbContext.cs
@@ -1,7 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace Dotnet_Core_EF_Api_Server.Persistence
 {
     public class VegaDbContext : DbContext 
     {
-        
+        public VegaDbContext(DbContextOptions<VegaDbContext> options) : base(options){}
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -2,12 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Dotnet_Core_EF_Api_Server.Persistence;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.SpaServices.Webpack;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
 
 namespace WebApplicationBasic
 {
@@ -28,6 +30,7 @@ namespace WebApplicationBasic
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<VegaDbContext>(option => option.UseSqlServer(Configuration.GetConnectionString("Default")));
             // Add framework services.
             services.AddMvc();
         }
