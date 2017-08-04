8c042cbc4c658b145ba2ab231aa6bf6594cfae09
sethbunke <sjb_strat@hotmail.com>
2017-08-04 01:11:07 +0000

    intial migration still in place - before removing

--- Persistence/VegaDbContext.cs
+++ Persistence/VegaDbContext.cs
@@ -1,3 +1,4 @@
+using Dotnet_Core_EF_Api_Server.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace Dotnet_Core_EF_Api_Server.Persistence
@@ -5,5 +6,7 @@ namespace Dotnet_Core_EF_Api_Server.Persistence
     public class VegaDbContext : DbContext 
     {
         public VegaDbContext(DbContextOptions<VegaDbContext> options) : base(options){}
+
+        public DbSet<Make> Make { get; set; }
     }
 }
