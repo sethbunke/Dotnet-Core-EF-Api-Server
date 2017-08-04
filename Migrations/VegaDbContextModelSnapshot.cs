4312f51e6e45668757630b4bdac652115b136cb3
sethbunke <sjb_strat@hotmail.com>
2017-08-04 01:03:26 +0000

    added ef migrations

---
+++ Migrations/VegaDbContextModelSnapshot.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Dotnet_Core_EF_Api_Server.Persistence;
+
+namespace DotnetCoreEFApiServer.Migrations
+{
+    [DbContext(typeof(VegaDbContext))]
+    partial class VegaDbContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ChangeDetector.SkipDetectChanges", "true")
+                .HasAnnotation("ProductVersion", "1.1.2")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+        }
+    }
+}
