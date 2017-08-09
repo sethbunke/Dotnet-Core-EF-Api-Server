3a9e1fab4b9c29581f3812c8616e2faea677c83e
sethbunke <sjb_strat@hotmail.com>
2017-08-09 02:11:44 +0000

    add ef constraints and apply

--- Models/Make.cs
+++ Models/Make.cs
@@ -1,11 +1,17 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Dotnet_Core_EF_Api_Server.Models
 {
+    [Table("Models")]
     public class Make
     {
+        
         public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
         public ICollection<Model> Models { get; set; }
 
--- Models/Model.cs
+++ Models/Model.cs
@@ -1,8 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Dotnet_Core_EF_Api_Server.Models
 {
     public class Model
     {
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
 
         public Make Make { get; set; } 
