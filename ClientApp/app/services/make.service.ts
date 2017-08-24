f6cfa5241652c38f8716528867a197343b3757c2
sethbunke <sjb_strat@hotmail.com>
2017-08-24 02:05:56 +0000

    makes dropdown working

---
+++ ClientApp/app/services/make.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class MakeService {
+
+  constructor(private http: Http) { }
+
+  getMakes() {
+    
+    let result = this.http.get('/api/makes')
+      .map(res => res.json());
+    return result
+  }
+
+}
