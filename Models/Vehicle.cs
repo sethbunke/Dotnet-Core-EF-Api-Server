0ff1b25c6385f641995d466c9efab05e6fa4f1d5
sethbunke <sjb_strat@hotmail.com>
2017-08-29 02:08:39 +0000

    vehicle complete

--- Models/Vehicle.cs
+++ Models/Vehicle.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel.DataAnnotations;
@@ -10,11 +11,20 @@ namespace Dotnet_Core_EF_Api_Server.Models
     public class Vehicle
     {
         public int Id { get; set; }
-        
+        public int ModelId { get; set; }
+        public Model Model { get; set; }
+        public bool IsRegistered { get; set; }
         [Required]
         [StringLength(255)]
-        public string Name { get; set; }
+        public string ContactName { get; set; }
 
+        [StringLength(255)]
+        public string ContactEmail { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string ContactPhone { get; set; }
+        public DateTime LastUpdate { get; set; }
         public ICollection<VehicleFeature> Features { get; set; }
 
         public Vehicle()
